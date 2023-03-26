259de642693a91bb8ad2b4154ff730d1fa6dcbf3
B.S Felipe Jeon <junbs95@gmail.com>
2023-03-26 17:38:03 +0900

    Add react graph

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
 import { GetStaticProps } from "next";
-
 export default function Home({
   allPostsData,
 }: {
@@ -20,6 +19,7 @@ export default function Home({
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
