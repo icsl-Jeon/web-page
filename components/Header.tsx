47c394d70d2e7386727cced40823e4c52b1dbbc0
Boseong Jeon <junbs95@gmail.com>
2023-04-02 21:32:53 +0900

    Introduce matchMedia

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,63 +1,74 @@
 import React, { useState, useEffect } from "react";
 import { PageCategory } from "../lib/types";
 import Link from "next/link";
-import { el } from "date-fns/locale";
 
 export default function Header({ category }: { category?: PageCategory }) {
   const [collapse, setCollapse] = useState(true);
-
-  const [width, setWidth] = useState(0);
+  const [isPortrait, setIsPortrait] = useState(true);
 
   useEffect(() => {
-    function handleResize() {
-      setWidth(window.outerWidth);
-    }
-    window.addEventListener("resize", handleResize);
-    window.addEventListener("orientationchange", handleResize);
+    if (typeof window === "undefined") return;
+    const mediaQuery = window.matchMedia("(orientation: portrait)");
 
-    handleResize();
+    function handleOrientationChange(event: MediaQueryListEvent) {
+      setIsPortrait(event.matches);
+    }
+
+    setIsPortrait(mediaQuery.matches);
+
+    mediaQuery.addEventListener("change", handleOrientationChange);
 
     return () => {
-      window.removeEventListener("orientationchange", handleResize);
-      window.addEventListener("resize", handleResize);
+      mediaQuery.removeEventListener("change", handleOrientationChange);
     };
   }, []);
 
   useEffect(() => {
-    if (width >= 500) setCollapse(false);
-    else setCollapse(true);
-  }, [width]);
+    if (isPortrait) {
+      setCollapse(true);
+    } else {
+      setCollapse(false);
+    }
+  }, [isPortrait]);
 
   return (
-    <div className=" flex flex-col sm:flex-row bg-slate-50 w-full shadow-md p-2  items-center ">
+    <div
+      className={`flex ${
+        isPortrait ? "flex-col" : "flex-row"
+      }   bg-slate-50 w-full shadow-md p-2  items-center `}
+    >
       <div className="flex flex-row justify-between  sm:ml-4 sm:mr-8 items-center  ">
-        <button
-          className="block sm:hidden"
-          onClick={() => {
-            setCollapse(!collapse);
-          }}
-        >
-          {" "}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="w-8 h-8 "
+        {isPortrait && (
+          <button
+            className=""
+            onClick={() => {
+              setCollapse(!collapse);
+            }}
           >
-            <path
-              fillRule="evenodd"
-              d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z"
-              clipRule="evenodd"
+            {" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-8 h-8 "
+            >
+              <path
+                fillRule="evenodd"
+                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        )}
+        {!isPortrait && (
+          <a href="/">
+            <img
+              src="/images/fox.png"
+              alt="brand logo"
+              className="object-contain h-12 hover:translate-x-1 hover:scale-105 transition-transform duration-300"
             />
-          </svg>
-        </button>
-        <a href="/">
-          <img
-            src="/images/fox.png"
-            alt="brand logo"
-            className="hidden sm:inline-block object-contain h-12 hover:translate-x-1 hover:scale-105 transition-transform duration-300"
-          />
-        </a>
+          </a>
+        )}
       </div>
 
       {Object.keys(PageCategory).map((key) => {
@@ -67,7 +78,7 @@ export default function Header({ category }: { category?: PageCategory }) {
         return (
           <div
             key={key}
-            className={`m-1 sm:p-3 p-1 ${
+            className={`m-1 p-3 ${
               collapse ? "hidden" : "inline"
             }  rounded-xl text-gray-600 hover:text-orange-300 transition-transform  transition-colors duration-200  text-xl ${
               isActive ? " text-orange-500" : ""
