7601e44914250e8681b13a87a6369ffbf5507f0c
Boseong Jeon <junbs95@gmail.com>
2023-04-02 22:00:19 +0900

    fix(integration): introduce portrait detect
    * Introduce matchMedia
    
    * fix shadow bug
    
    * Add width device rule

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,12 @@ import { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <Head> </Head>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
