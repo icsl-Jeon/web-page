b57f0a3914f32395d7582e67f777d1d61d8a7b6b
Boseong Jeon <junbs95@gmail.com>
2023-10-16 21:18:00 +0900

    Change tag css

--- components/tag-list.tsx
+++ components/tag-list.tsx
@@ -14,12 +14,12 @@ const Tag = ({
     <a
       href={href}
       className={clsx(
-        'rounded-md bg-gray-200 hover:!no-underline dark:bg-[#24272E]',
+        'rounded-lg bg-sky-100 hover:!no-underline dark:bg-[#24272E]',
         'flex py-1.5 px-2.5 text-sm font-medium transition-colors',
         href
           ? 'hover:text-black hover:dark:bg-[#15AFD04C] hover:dark:text-[#82E9FF]'
           : 'cursor-default',
-        isActive ? 'dark:bg-[#15AFD04C] dark:text-[#82E9FF]' : 'text-gray-500 dark:text-[#7F818C]',
+        isActive ? 'dark:bg-[#15AFD04C] dark:text-[#82E9FF] text-black' : 'text-gray-500 dark:text-[#7F818C]',
       )}
       {...props}
     >
